b3614d6ad46cd852d3dfc905120ca9b798c9bfcf
Ayush12ka <kakkarayush111@gmail.com>
2020-09-05 08:59:40 +0000

    rr

---
+++ rr_whyred.mk
@@ -0,0 +1,67 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit some common PixelExperience stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Inherit from whyred device
+$(call inherit-product, device/xiaomi/whyred/device.mk)
+
+# Inherit from custom vendor
+$(call inherit-product, vendor/MiuiCamera/config.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := whyred
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := rr_whyred
+PRODUCT_MODEL := Redmi Note 5 Pro
+
+# RR Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+RR_BUILDTYPE := Official
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
+IS_PHONE := true
+TARGET_MINIMAL_APPS := false
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+# Inherit OPScreen Recorder
+$(call inherit-product, vendor/apps/OPScreenRecord/config.mk)
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+TARGET_VENDOR_PRODUCT_NAME := whyred
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="whyred" \
+    PRODUCT_NAME="whyred" \
+
+# Build Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+
+#Build FP to be picked by both system and vendor
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
